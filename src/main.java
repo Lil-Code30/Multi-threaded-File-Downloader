a7e66c98ac8a23cfe69223247af3eadacb6df318
Ismael Loko <107557003+Lil-Code30@users.noreply.github.com>
2026-06-25 19:26:25 -0400

    feat(step3): Add concurrent downloader and sample PDFs
    Add three sample PDFs to downloads/ and refactor src/main.java to download files concurrently. Introduces an ExecutorService (fixed thread pool of 4) and runs each download in executor.execute(...) with a simple log of thread and filename. Adds imports, a TODO to validate user-provided URLs, and shuts down the executor after scheduling tasks. Notes a potential filename collision when using new Date().getTime() for names.

--- src/main.java
+++ src/main.java
@@ -7,24 +7,31 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class main {
     public static void main(String[] args) {
         System.out.println("===== Multi-threaded File Downloader ===== ");
+        System.out.println("NOTE: just '.pdf' files are acceptable");
 
         ArrayList<String> fileUrls = new ArrayList<>();
         Scanner input = new Scanner(System.in);
+
+        ExecutorService executor = Executors.newFixedThreadPool(4);
 
         System.out.print("How many files do you want to download? ");
         int n = input.nextInt();
 
         // prompting the user to enter the url link of the file to be downloaded
+        // TODO: add a verification for the url entered by the user
         for(int i = 0; i < n; i++){
             System.out.println("Please enter the URL of the file you want to download: ");
             fileUrls.add(input.next());
         }
 
-        // test Link : https://raw.githubusercontent.com/wususu/effective-resourses/master/Java/Java%20Concurrency%20in%20Practice.pdf
+        // test Link :
+        // https://raw.githubusercontent.com/wususu/effective-resourses/master/Java/Java%20Concurrency%20in%20Practice.pdf
         // https://www.cs.cmu.edu/afs/cs.cmu.edu/user/gchen/www/download/java/LearnJava.pdf
         // https://www.uvm.edu/~cbcafier/itpacs/itpacs_cafiero.pdf
 
@@ -37,16 +44,24 @@ public class main {
                 e.getMessage();
             }
         }
-
+        // TODO: Progress tracking uses AtomicInteger and ConcurrentHashMap
         // downloading all files from the URLs provided (FileUrls)
         for(String fileUrl : fileUrls){
-            try{
-                InputStream in = URI.create(fileUrl).toURL().openStream();
-                String filename = new Date().getTime() + ".pdf";
-                Files.copy(in, Paths.get("./downloads/"+filename));
-            }catch (IOException ex) {
-                ex.printStackTrace();
-            }
+            executor.execute(() -> {
+                try{
+                    InputStream in = URI.create(fileUrl).toURL().openStream();
+
+                    // Note: It can happen that two threads create the new Date() at the same time
+                    // causing the FileExistException when trying to copy the file with the .getTime() from the date
+                    String filename = new Date().getTime() + ".pdf";
+                    Files.copy(in, Paths.get("./downloads/"+filename));
+                    System.out.println(Thread.currentThread().getName() + " is downloading " + filename);
+                }catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            });
         }
+
+        executor.shutdown();
     }
 }
